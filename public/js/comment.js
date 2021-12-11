c72f4a4df4199b11b522925bb1538cc22f9c7b8d
Jalvarez025 <90490928+Jalvarez025@users.noreply.github.com>
2021-12-10 21:50:24 -0600

    M

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,20 +1,25 @@
-const commentFormHandler = async function(event){
-    event.preventDefault()
-    const postId = document.querySelector('input[name="post-id"]').value
-    const body = document.querySelector('textarea[name="comment-body"]').value
+const commentFormHandler = async function(event) {
+  event.preventDefault();
 
-    if(body){
-        await fetch('/api/comment', {
-            method: 'POST',
-            body: JSON.stringify({
-                postId, body
-            }),
-            headers: {'Content-Type': 'application/json'}
-        })
-        document.location.reload()
-    }
-}
+  const postId = document.querySelector('input[name="post-id"]').value;
+  const body = document.querySelector('textarea[name="comment-body"]').value;
+
+  if (body) {
+    await fetch('/api/comment', {
+      method: 'POST',
+      body: JSON.stringify({
+        postId,
+        body
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+
+    document.location.reload();
+  }
+};
 
 document
-    .querySelector('#new-comment-form')
-    .addEventListener('submit', commentFormHandler)
+  .querySelector('#new-comment-form')
+  .addEventListener('submit', commentFormHandler);
